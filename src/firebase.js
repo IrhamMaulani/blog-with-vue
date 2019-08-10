d6318346bf0a186862a2f5c8258386d7311b0b55
Irham Maulani <irhammaulani@gmail.com>
2019-08-11 00:26:02 +0800

    fix

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-
-
-firebase.initializeApp({
-    projectId: 'projectId',
-    databaseURL: 'databaseUrl'
-});
-export const db = firebase.firestore()
