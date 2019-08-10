736ea40d9f64fe92141992dfb66b201a3ef3d581
Irham Maulani <irhammaulani@gmail.com>
2019-08-11 00:22:11 +0800

    change firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/firestore'
 
 
 firebase.initializeApp({
-    projectId: 'learn-vue-db76a',
-    databaseURL: 'https://learn-vue-db76a.firebaseio.com/'
+    projectId: 'projectId',
+    databaseURL: 'databaseUrl'
 });
 export const db = firebase.firestore()
